f6a9e21c23d34f5e47da5837014845957ecc894c
Jenifer19IFC <jenifergoedert10@gmail.com>
2021-10-21 11:27:37 -0300

    update

--- src/ConeMain.java
+++ src/ConeMain.java
@@ -1,24 +1,12 @@
-import java.util.Scanner;
+
 
 public class ConeMain {
 
 	public static void main(String[] args) {
 		
-		Cone c = new Cone (0);
-		Scanner input=new Scanner (System.in);
-	
-		System.out.println("Digite o raio: ");
-		c.setRaio(input.nextFloat());
-	
-		System.out.println("Digite o altura: ");
-		c.setAlt(input.nextFloat());
-	
-		System.out.println("Tipo de tinta: 1 - Tipo 1; 2 - Tipo 2; 3 - Tipo 3.");
-		c.setTipo(input.nextInt());
+		Cone c = new Cone (6, 5, 1);
 		
 		System.out.println(c);
-		
-		input.close();
 		
 	}
 
